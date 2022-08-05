556b3baf1f75da864f6fa008bc524023ead7a37c
fedecanesa <federicocanesa@gmail.com>
2022-08-05 15:13:42 -0300

    The list of menu titles is added

---
+++ src/constants/menuEnum.js
@@ -0,0 +1,11 @@
+export const menuTitles = {
+    HOME: 'inicio',
+    SERIES: 'series',
+    MOVIES: 'películas',
+    RECENTLY_ADDED: 'agregadas recientemente',
+    POPULAR: 'populares',
+    MY_MOVIES: 'mis películas',
+    MY_LIST: 'mi lista',
+    ADD_MOVIES: 'agregar película',
+    SIGN_OFF: 'cerrar sesión',
+}
