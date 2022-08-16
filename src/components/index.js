ad2b5f240b80e787ed84502dc5c2467343a9485a
fedecanesa <federicocanesa@gmail.com>
2022-08-16 18:21:39 -0300

    the index for the components is created

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Button from "./Button/Button";
+import Dropdown from "./Dropdown/Dropdown";
+import Layout from "./Layout/Layout";
+import Loader from "./Loader/Loader";
+import MainTitle from "./MainTitle/MainTitle";
+import Menu from "./Menu/Menu";
+import Modal from "./Modal/Modal";
+import Movie from "./Movie/Movie";
+import MovieList from "./MovieList/MovieList";
+import Navbar from "./Navbar/Navbar";
+import Progressbar from "./Progressbar/Progressbar";
+
+export {
+    Button,
+    Dropdown,
+    Layout,
+    Loader,
+    MainTitle,
+    Menu,
+    Modal,
+    Movie,
+    MovieList,
+    Navbar,
+    Progressbar
+}
