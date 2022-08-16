57f7aa7814f62a4b6f6812b23fd4a233d9a3deaf
fedecanesa <federicocanesa@gmail.com>
2022-08-16 18:03:22 -0300

    Add filter and menu enums

---
+++ src/constants/filterEnum.js
@@ -0,0 +1,4 @@
+export const filterEnum = [
+    'populares',
+    'mis peliculas'
+]
--- src/constants/menuEnum.js
+++ src/constants/menuEnum.js
@@ -1,11 +1,11 @@
-export const menuTitles = {
-    HOME: 'inicio',
-    SERIES: 'series',
-    MOVIES: 'películas',
-    RECENTLY_ADDED: 'agregadas recientemente',
-    POPULAR: 'populares',
-    MY_MOVIES: 'mis películas',
-    MY_LIST: 'mi lista',
-    ADD_MOVIES: '+ agregar película',
-    SIGN_OFF: 'cerrar sesión',
-}
+export const menuTitles = [
+    { HOME: 'inicio', value: 'inicio' },
+    { SERIES: 'series', value: 'series' },
+    { MOVIES: 'películas', value: 'películas' },
+    { RECENTLY_ADDED: 'agregadas recientemente', value: 'agregadas recientemente' },
+    { POPULAR: 'populares', value: 'populares' },
+    { MY_MOVIES: 'mis películas', value: 'mis películas' },
+    { MY_LIST: 'mi lista', value: 'mi lista' },
+    { ADD_MOVIES: 'agregar película', value: 'agregar película' },
+    { SIGN_OFF: 'cerrar sesión', value: 'cerrar sesión' },
+]
