324ea58377e419de9832d9a922a2652e4af4a9e1
fedecanesa <federicocanesa@gmail.com>
2022-08-16 17:57:05 -0300

    Add an util file to export the images

---
+++ src/utils/image.js
@@ -0,0 +1,33 @@
+import arrow from '../assets/arrow.png';
+import close from '../assets/cerrar.png';
+import check from '../assets/check.png';
+import fill from '../assets/fill.png';
+import menu from '../assets/menu.png';
+import notification from '../assets/notifications.png';
+import oval from '../assets/oval.png';
+import play from '../assets/play.png';
+import play2 from '../assets/play2.png';
+import play3 from '../assets/play3.png';
+import play4 from '../assets/play4.png';
+import plus from '../assets/plus.png';
+import rectangle from '../assets/rectangle.png';
+import star from '../assets/star.png';
+import userProfile from '../assets/userProfile.png';
+
+export {
+    arrow,
+    close,
+    check,
+    fill,
+    menu,
+    notification,
+    oval,
+    play,
+    play2,
+    play3,
+    play4,
+    plus,
+    rectangle,
+    star,
+    userProfile
+}
